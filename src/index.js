e4811df2cb547afa523214fe68a8b80d0e732e4c
MarcinBalejko <mbbm9@o2.pl>
2020-08-28 01:21:56 +0200

    :white_check_mark: Added action creators

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import "./index.css";
 import App from "./App";
-import reducer from "./store/reducer";
+import burgerBuilderReducer from "./store/reducers/burgerBuilderReducer";
 
 const store = createStore(
-  reducer,
+  burgerBuilderReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
