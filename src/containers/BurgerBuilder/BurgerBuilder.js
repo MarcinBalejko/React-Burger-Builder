c0567f09fd9650eb067a3cfc92b2e1808b8d8be7
MarcinBalejko <mbbm9@o2.pl>
2020-08-28 22:53:39 +0200

    :white_check_mark: Initializing ingredients in the Burger Builder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,6 +23,7 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     console.log(this.props);
+    this.props.onInitIngredients();
   }
 
   updatePurchaseState(ingredients) {
@@ -102,7 +103,11 @@ class BurgerBuilder extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { ings: state.ingredients, price: state.totalPrice };
+  return {
+    ings: state.ingredients,
+    price: state.totalPrice,
+    error: state.error,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
@@ -111,6 +116,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(burgerBuilderActions.addIngredient(ingName)),
     onIngredientRemoved: (ingName) =>
       dispatch(burgerBuilderActions.removeIngredient(ingName)),
+    onInitIngredients: () => dispatch(burgerBuilderActions.initIngredients()),
   };
 };
 
