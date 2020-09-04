69041210038bb5a48d3687a208f962cf8c6d3027
MarcinBalejko <mbbm9@o2.pl>
2020-09-04 16:07:12 +0200

    :recycle: Code refactored, deleted unnecessary console logs

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,9 +5,6 @@ import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-  // optional console log
-  console.log(props);
-  // ^^^
   let transformedIngredients = Object.keys(props.ingredients)
     .map((igKey) => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
