c22a2d15db0c01d7d8ee0fa1ca139ef6f30f8349
MarcinBalejko <mbbm9@o2.pl>
2020-09-08 01:21:51 +0200

    :recycle: Preparing for heroku deployment

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 
 const composeEnhancers =
-  process.env.REACT_APP_NODE_ENV === "development"
+  process.env.NODE_ENV === "development"
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     : null || compose;
 
